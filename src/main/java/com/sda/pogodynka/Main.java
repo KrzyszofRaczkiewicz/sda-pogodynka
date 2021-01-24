0b6f425234c0e446bcdf1d64bbbc6bb5bd91708b
KrzyszofRaczkiewicz <72334687+KrzyszofRaczkiewicz@users.noreply.github.com>
2021-01-24 09:35:17 +0100

    menu - full full

--- src/main/java/com/sda/pogodynka/Main.java
+++ src/main/java/com/sda/pogodynka/Main.java
@@ -6,32 +6,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
 
-        while (true) {
-            MenuClass.getMenu();
-
-            String menu = scanner.next();
-            switch (menu) {
-                case "1": // dodaj lokalizacje
-                    System.out.println("Dodajemy lokalizację");
-                    System.out.println("----------------------------------");
-                    break;
-                case "2": // lista lokalizacji
-                    System.out.println("Lista lokalizacji");
-                    System.out.println("----------------------------------");
-                    break;
-                case "3": // aktualizacja danych lokalizacji
-                    System.out.println("Aktualizacja danych");
-                    System.out.println("----------------------------------");
-                    break;
-                case "0":
-                    System.out.println("Sajonara!!!");
-                    return;
-                default:
-                    System.out.println("czytanie ze zrozumieniem leży nie?");
-            }
-        }
-
+        MenuClass.getMenu();
     }
 }
--- src/main/java/com/sda/pogodynka/frontend/MenuClass.java
+++ src/main/java/com/sda/pogodynka/frontend/MenuClass.java
@@ -1,12 +1,41 @@
 package com.sda.pogodynka.frontend;
 
+import java.util.Scanner;
+
 public class MenuClass {
 
-    public static void getMenu(){
+    public static void getMenuInfo(){
         System.out.println("POGODYNKA 1.0");
         System.out.println("1 - Dodaj lokalizacje do bazy danych");
         System.out.println("2 - Lista zapisanych lokalizacji");
         System.out.println("3 - Aktualizuj dane lokalizacji");
         System.out.println("0 - ZAKOŃCZ DZIAŁANIE PROGRAMU");
+    }
+
+    public static void getMenu(){
+        while (true) {
+            getMenuInfo();
+            Scanner scanner = new Scanner(System.in);
+            String menu = scanner.next();
+            switch (menu) {
+                case "1": // dodaj lokalizacje
+                    System.out.println("Dodajemy lokalizację");
+                    System.out.println("----------------------------------");
+                    break;
+                case "2": // lista lokalizacji
+                    System.out.println("Lista lokalizacji");
+                    System.out.println("----------------------------------");
+                    break;
+                case "3": // aktualizacja danych lokalizacji
+                    System.out.println("Aktualizacja danych");
+                    System.out.println("----------------------------------");
+                    break;
+                case "0":
+                    System.out.println("Sajonara!!!");
+                    return;
+                default:
+                    System.out.println("nie ze mną te numery Bruner....\n\n sprobuj jeszcze raz...");
+            }
+        }
     }
 }
